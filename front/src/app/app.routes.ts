2f5315fd7b2f1abf4e4022df66d8bcc3f051a1d1
dguerrem <99349002+dguerrem@users.noreply.github.com>
2025-01-18 13:06:27 +0100

    feat(): Inicio - Diseño Inicial
    Primera toma contacto ng-circle-progress
    Formato bootstrap

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: InicioComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: ""
+    }
+];
--- front/src/app/inicio/inicio.component.ts
+++ front/src/app/inicio/inicio.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { CircleProgressOptions, NgCircleProgressModule } from 'ng-circle-progress';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgCircleProgressModule],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrl: './inicio.component.css',
+  providers: [
+    {
+      provide: CircleProgressOptions,
+      useValue: {
+        radius: 60,
+        outerStrokeWidth: 8,
+        innerStrokeWidth: 4,
+        outerStrokeColor: '#ff6f61',
+        innerStrokeColor: '#f0f0f0',
+        animationDuration: 300,
+        animation: true,
+      },
+    },
+  ],
 })
 export class InicioComponent {
+  stats = [
+    { title: 'Libros Leídos', value: 12, total: 20 },
+    { title: 'Páginas Leídas', value: 3500, total: 5000 },
+    { title: 'Nota Media', value: 4.2, total: 5 },
+    { title: 'Libros En este Mes', value: 21, total: 30 },
+  ];
 
+  startNewBook() {
+    console.log('Starting a new book...');
+  }
 }
